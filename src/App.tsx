b2db2b39720ad4fd5b84751bf4b1c1cf0840a1f1
Muhammad Rifki <69866073+rifki-sgnic@users.noreply.github.com>
2023-02-13 15:21:55 +0700

    feature: add bottom nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {
-  Cart,
-  Confirmation,
-  FoodList,
-  Home,
-  Payment,
-  Profile,
-} from "./presentation";
-
-export type StackParams = {
-  Home: any;
-  Cart: {
-    item: string;
-  };
-  Profile: undefined;
-  FoodList: undefined;
-  Payment: undefined;
-  Confirmation: undefined;
-};
-
-const Stack = createNativeStackNavigator<StackParams>();
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Router from "./router";
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Cart"
-          component={Cart}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="FoodList"
-          component={FoodList}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Payment"
-          component={Payment}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Confirmation"
-          component={Confirmation}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </>
   );
 };
 
